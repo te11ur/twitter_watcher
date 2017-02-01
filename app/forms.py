2ffc6fd5822ed996e7e25292f7f49ded6c2b750d
Aziz Kalandarov <aziz.kalandarov@kodex.lv>
2017-02-01 20:14:29 +0200

    create service tables

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
-from flask_wtf import Form
+from flask_wtf import FlaskForm
 from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
-class LoginForm(Form):
+class LoginForm(FlaskForm):
     openid = TextField('openid', validators = [Required()])
     remember_me = BooleanField('remember_me', default = False)
--- app/models.py
+++ app/models.py
@@ -1,23 +1,72 @@
 from app import db
+#from enum import Enum
 
-ROLE_USER = 0
-ROLE_ADMIN = 1
+#class StatusEnum(Enum):
+#    off = "off"
+#    on = "on"
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key = True)
     nickname = db.Column(db.String(64), index = True, unique = True)
     email = db.Column(db.String(120), index = True, unique = True)
-    role = db.Column(db.SmallInteger, default = ROLE_USER)
-    posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+   #role = db.Column(db.SmallInteger, default = ROLE_USER)
+   # posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+
+    def is_authenticated(self):
+        return True
+
+    def is_active(self):
+        return True
+
+    def is_anonymous(self):
+        return False
+
+    def get_id(self):
+        return unicode(self.id)
 
     def __repr__(self):
         return '<User %r>' % (self.nickname)
 
-class Post(db.Model):
+class Service(db.Model):
     id = db.Column(db.Integer, primary_key = True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    command = db.Column(db.String(100), nullable=False)
+    repository = db.Column(db.Boolean, default=False)
+    push = db.Column(db.Boolean, default=False)
+    status = db.Column(db.Enum("off", "on"))
+    enabled = db.Column(db.Boolean, default=False)
+    watchers = db.relationship("Watcher", order_by=Watcher.id, back_populates="service", cascade="all, delete, delete-orphan")
 
     def __repr__(self):
-        return '<Post %r>' % (self.body)
+        return "<Service(command='%s')>" % self.command
+
+class Watcher(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    service_id = db.Column(db.Integer, db.ForeignKey('service.id'))
+    service = db.relationship("Service", back_populates="watchers")
+    application_id = db.Column(db.Integer, db.ForeignKey('application.id'))
+    application = db.relationship("Application", back_populates="watchers")
+    repositories = db.relationship("Repository", order_by=Repository.id, back_populates="watcher")
+   
+    def __repr__(self):
+        return "<Watcher(name='%s')>" % self.name
+
+class Application(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    watchers = db.relationship("Watcher", order_by=Watcher.id, back_populates="application")
+
+    def __repr__(self):
+        return "<Application(name='%s')>" % self.name
+
+class Repository(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    watcher_id = db.Column(db.Integer, db.ForeignKey('watcher.id'))
+    watcher = db.relationship("Watcher", back_populates="repositories")
+    create = db.Column(db.Time)
+    add = db.Column(db.Time)
+    push = db.Column(db.Time)
+
+    def __repr__(self):
+        return "<Repository(create='%s')>" % self.create
+
