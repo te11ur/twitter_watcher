096cca013404827b1bc4735b95712b4a8d1e14ea
Aziz Kalandarov <aziz.kalandarov@kodex.lv>
2017-02-03 14:09:43 +0200

    add watchers and services

--- app/forms.py
+++ app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import TextField, BooleanField, HiddenField
+from wtforms import TextField, BooleanField, HiddenField, BooleanField, SelectField
 from wtforms.validators import Required
 
 class LoginForm(FlaskForm):
@@ -10,3 +10,18 @@ class ApplicationForm(FlaskForm):
 	id = HiddenField('Id')
 	delete = HiddenField('delete')
 	name = TextField('Name', validators = [Required()])
+
+class ServiceForm(FlaskForm):
+	id = HiddenField('Id')
+	delete = HiddenField('delete')
+	command = TextField('Command', validators = [Required()])
+	repository = BooleanField('Repository', default = True)
+	push = BooleanField('Push', default = False)
+	enabled = BooleanField('Enabled', default = True)
+
+class WatcherForm(FlaskForm):
+	id = HiddenField('Id')
+	delete = HiddenField('delete')
+	name = TextField('Name', validators = [Required()])
+	application = SelectField('Application', validators = [Required()], coerce = int)
+	service = SelectField('Service', validators = [Required()], coerce = int)
--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from flask import render_template, flash, redirect, session, url_for, request, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm, oid
-from forms import LoginForm, ApplicationForm
-from models import User, Application
+from forms import LoginForm, ApplicationForm, ServiceForm, WatcherForm
+from models import User, Application, Service, Watcher
 from datetime import datetime
 from config import POSTS_PER_PAGE
 
@@ -56,18 +56,30 @@ def applications(page=1):
 	#user = g.user
 	form = ApplicationForm()
 	if form.validate_on_submit():
-		if form.delete.data > 0:
-			application = db.session.query(Application).get(form.delete.data)
+
+		try:
+			deleteId = int(form.delete.data);
+		except ValueError as e:
+			deleteId = 0
+
+		try:
+			editId = int(form.id.data);
+		except ValueError as e:
+			editId = 0
+
+		if deleteId > 0:
+			application = db.session.query(Application).get(deleteId)
 			if application is None:
-				flash('Cannot delete application: ' + form.delete.data)
+				flash('Cannot delete application: ' + str(deleteId))
 			else:
 				db.session.delete(application)
+				db.session.commit()
 			return redirect(url_for('applications', page = page))
 
-		if form.id.data	> 0:
-			application = session.query(Application).get(form.id.data)
+		if editId > 0:
+			application = db.session.query(Application).get(editId)
 			if application is None:
-				flash('Cannot edit application: ' + form.id.data)
+				flash('Cannot edit application: ' + str(editId))
 			else:
 				application.name = form.name.data
 				db.session.commit()
@@ -82,6 +94,109 @@ def applications(page=1):
 	applications = Application.query.paginate(page, POSTS_PER_PAGE, False);
 
 	return render_template('list.html', pages = pages, form = form, elements = applications, fields = ['id', 'name'], title = 'Application', model = 'application', route = 'applications')
+
+
+@app.route('/admin/services', methods = ['GET', 'POST'])
+@app.route('/admin/services/<int:page>', methods = ['GET', 'POST'])
+#@login_required
+def services(page=1):
+	#user = g.user
+	form = ServiceForm()
+	if form.validate_on_submit():
+
+		try:
+			deleteId = int(form.delete.data);
+		except ValueError as e:
+			deleteId = 0
+
+		try:
+			editId = int(form.id.data);
+		except ValueError as e:
+			editId = 0
+
+		if deleteId > 0:
+			service = db.session.query(Service).get(deleteId)
+			if service is None:
+				flash('Cannot delete service: ' + str(deleteId))
+			else:
+				db.session.delete(service)
+				db.session.commit()
+			return redirect(url_for('services', page = page))
+
+		if editId > 0:
+			service = db.session.query(Service).get(editId)
+			if service is None:
+				flash('Cannot edit service: ' + str(editId))
+			else:
+				service.command = form.command.data
+				service.repository = form.repository.data
+				service.push = form.push.data
+				service.enabled = form.enabled.data
+				db.session.commit()
+			return redirect(url_for('services', page = page))
+			
+		service = Service(command = form.command.data, repository = form.repository.data, push = form.push.data, enabled = form.enabled.data, status = 'off')
+		db.session.add(service)
+		db.session.commit()
+		flash('Your services is now live!')
+		return redirect(url_for('services', page = page))
+	
+	services = Service.query.paginate(page, POSTS_PER_PAGE, False);
+
+	return render_template('list.html', pages = pages, form = form, elements = services, fields = ['id', 'command', 'repository', 'push', 'enabled', 'status'], title = 'Service', model = 'service', route = 'services')
+
+
+@app.route('/admin/watchers', methods = ['GET', 'POST'])
+@app.route('/admin/watchers/<int:page>', methods = ['GET', 'POST'])
+#@login_required
+def watchers(page=1):
+	#user = g.user
+	form = WatcherForm()
+	form.application.choices = [(a.id, a.name) for a in Application.query.order_by('id')]
+	form.service.choices = [(s.id, s.command) for s in Service.query.order_by('id')]
+
+	if form.validate_on_submit():
+
+		try:
+			deleteId = int(form.delete.data);
+		except ValueError as e:
+			deleteId = 0
+
+		try:
+			editId = int(form.id.data);
+		except ValueError as e:
+			editId = 0
+
+		if deleteId > 0:
+			watcher = db.session.query(Watcher).get(deleteId)
+			if watcher is None:
+				flash('Cannot delete watcher: ' + str(deleteId))
+			else:
+				db.session.delete(watcher)
+				db.session.commit()
+			return redirect(url_for('watchers', page = page))
+
+		if editId > 0:
+			watcher = db.session.query(Watcher).get(editId)
+			if watcher is None:
+				flash('Cannot edit watcher: ' + str(editId))
+			else:
+				watcher.name = form.name.data
+				watcher.application = form.application.data
+				watcher.service = form.service.data
+				db.session.commit()
+			return redirect(url_for('watchers', page = page))
+			
+		watcher = Watcher(name = form.name.data, application = form.application.data, service = form.service.data)
+		db.session.add(watcher)
+		db.session.commit()
+		flash('Your watcher is now live!')
+		return redirect(url_for('watchers', page = page))
+	
+	watchers = Watcher.query.paginate(page, POSTS_PER_PAGE, False);
+
+	return render_template('list.html', pages = pages, form = form, elements = watchers, fields = ['id', 'name', 'application', 'service'], title = 'Watcher', model = 'watcher', route = 'watchers')
+
 
 @app.route('/admin/login', methods = ['GET', 'POST'])
 #@oid.loginhandler
