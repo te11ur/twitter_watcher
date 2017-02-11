f836f1eea9df2ba8d207dc9f1ca5035a73644913
Aziz Kalandarov <tiller100@yandex.ru>
2017-02-11 12:15:58 +0000

    add dep

--- app/__init__.py
+++ app/__init__.py
@@ -3,8 +3,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import basedir
+from werkzeug.contrib.fixers import ProxyFix
 
 app = Flask(__name__)
+app.wsgi_app = ProxyFix(app.wsgi_app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 lm = LoginManager()
--- run.py
+++ run.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python
 from app import app
-from werkzeug.contrib.fixers import ProxyFix
-from config import SERVER_HOST, SERVER_PORT, DEBUG
 
 if __name__ == '__main__':
+	from config import SERVER_HOST, SERVER_PORT, DEBUG
 	app.run(host=SERVER_HOST, port=SERVER_PORT, debug = DEBUG)
